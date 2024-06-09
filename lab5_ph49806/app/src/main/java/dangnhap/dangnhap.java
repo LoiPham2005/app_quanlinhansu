45821b703a87b3740d9d02b8c09f06f2c8d67682
LoiPham2005 <phamducloi919@gmail.com>
2024-06-09 10:55:03 +0700

    "add9-6"

--- lab5_ph49806/app/src/main/java/dangnhap/dangnhap.java
+++ lab5_ph49806/app/src/main/java/dangnhap/dangnhap.java
@@ -1,9 +1,11 @@
 package dangnhap;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,6 +22,7 @@ public class dangnhap extends AppCompatActivity {
 
     EditText edtName, edtPassword;
     Button btnLogin, btnRegister;
+    CheckBox remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,7 @@ public class dangnhap extends AppCompatActivity {
         edtPassword = findViewById(R.id.edt_password);
         btnLogin = findViewById(R.id.btn_login);
         btnRegister = findViewById(R.id.btn_register);
+        remember = findViewById(R.id.cb_luuMK);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,16 +64,33 @@ public class dangnhap extends AppCompatActivity {
             public void onClick(View v) {
                 String name = edtName.getText().toString();
                 String password = edtPassword.getText().toString();
+                boolean check = remember.isChecked();
 
                 if (name.isEmpty() || password.isEmpty()) {
                     Toast.makeText(dangnhap.this, "Vui lòng nhập tên đăng nhập và mật khẩu", Toast.LENGTH_SHORT).show();
                 } else {
                     // Xử lý logic đăng nhập ở đây
+                    if(check) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("fullName", name);
+                        editor.putString("password", password);
+                        editor.apply();
+
+                        SharedPreferences sharedPreferences2 = getSharedPreferences("data", MODE_PRIVATE);
+                        String fullName2 = sharedPreferences2.getString("fullName", "");
+                        String password2 = sharedPreferences2.getString("password", "");
+//
+                    }
+
                     Toast.makeText(dangnhap.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(dangnhap.this, Home.class));
                 }
             }
+
+
         });
 
     }
+
 }
